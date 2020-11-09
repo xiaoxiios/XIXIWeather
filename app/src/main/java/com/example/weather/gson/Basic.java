468ae34a846083c4be6c706ab674d57617606c2f
xicheng <1219541190@qq.com>
2020-11-09 16:43:39 +0800

    可以显示单个城市的天气信息了

--- app/src/main/java/com/example/weather/gson/Basic.java
+++ app/src/main/java/com/example/weather/gson/Basic.java
@@ -7,7 +7,7 @@ public class Basic {
     public String cityName;
 
     @SerializedName("id")
-    public String weatherID;
+    public String weatherId;
 
     public Update update;
     public class Update{
